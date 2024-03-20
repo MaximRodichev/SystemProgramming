8c3f24da42f046224281744f513588e9f671772f
Maxim Rodichev <vymasfyaya@gmail.com>
2024-03-20 22:59:15 +0300

    External

--- Practice 4 ~ 14.02.2024/Practice 4 ~ 14.02.2024/Program.cs
+++ Practice 4 ~ 14.02.2024/Practice 4 ~ 14.02.2024/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Instructs;
+using static Instructs.Instructions;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Practice_4___14._02._2024
 {
@@ -9,15 +13,33 @@ namespace Practice_4___14._02._2024
         {
             void Task1()
             {
-                Brevno BrevnoPervoe = new Brevno(Instructions.inputInt("Длина первого бревна: "));
-                Brevno BrevnoVtoroe = new Brevno(Instructions.inputInt("Длина второго бревна: "));
-                double maxSize;
-                double t = BrevnoVtoroe.L>BrevnoPervoe.L ? BrevnoVtoroe.L : BrevnoVtoroe.L;
-                while(true)
+                infoWithStrikeLine("Task 1 with 2 Brevnos");
+                double MaxSize = 0;
+                int[] brevnoSizes = { 15, 14, 13, 14, 12, 6, 3 };
+                List<Brevno> brevnos= new List<Brevno>() {};
+                //do { brevnos.Add(new Brevno(inputInt($"Длина {brevnos.Count + 1} бревна: "))); }
+                //while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+                do { brevnos.Add(new Brevno(brevnoSizes[brevnos.Count])); } 
+                while(brevnoSizes.Length != brevnos.Count);
+                foreach(var brevno in brevnos) { info($"{brevno}"); }
+                bool isOpen = true;
+                double max = brevnos.Select(x=>x.L).Max();
+                double check = max;
+                double truth = 0.25;
+                int count = 7;
+                do
                 {
-                    BrevnoVtoroe
+                    int countHere = brevnos.Where(x=>x.L>=check).Count();
+                    info($"countHere: {countHere}\tcheck: {check}\tmax: {max}\tcount: {count}");
+                    if (countHere < count && isOpen) { max /= 2; check = 0; isOpen = false; }
+                    else if (countHere > count) { check = (check + max) / 2; isOpen = true; }
+                    ReadLine();
                 }
+                while (true);
             }
+            while(true) { Task1(); }
+            ReadLine();
+
         }
     }
 }
