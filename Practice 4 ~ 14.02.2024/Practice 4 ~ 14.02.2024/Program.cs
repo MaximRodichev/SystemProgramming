0fa6796ca195912f8f3a0af58e326ded863ee819
Maxim Rodichev <vymasfyaya@gmail.com>
2024-03-22 10:22:38 +0300

    Practice 4 Complete Brevnos

--- Practice 4 ~ 14.02.2024/Practice 4 ~ 14.02.2024/Program.cs
+++ Practice 4 ~ 14.02.2024/Practice 4 ~ 14.02.2024/Program.cs
@@ -4,6 +4,7 @@ using static Instructs.Instructions;
 using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 
 namespace Practice_4___14._02._2024
 {
@@ -13,31 +14,80 @@ namespace Practice_4___14._02._2024
         {
             void Task1()
             {
+                const int count = 7;
+                const double truth = 0.1;
                 infoWithStrikeLine("Task 1 with 2 Brevnos");
                 double MaxSize = 0;
-                int[] brevnoSizes = { 15, 14, 13, 14, 12, 6, 3 };
+                //int[] brevnoSizes = { 12,13,7,8,2,3 };
+                int[] brevnoSizes = { 14, 12, 13, 14, 15, 6, 3 };
+                //int[] brevnoSizes = { 9,3};
+
                 List<Brevno> brevnos= new List<Brevno>() {};
-                //do { brevnos.Add(new Brevno(inputInt($"Длина {brevnos.Count + 1} бревна: "))); }
-                //while (Console.ReadKey().Key != ConsoleKey.Escape) ;
                 do { brevnos.Add(new Brevno(brevnoSizes[brevnos.Count])); } 
                 while(brevnoSizes.Length != brevnos.Count);
                 foreach(var brevno in brevnos) { info($"{brevno}"); }
-                bool isOpen = true;
                 double max = brevnos.Select(x=>x.L).Max();
-                double check = max;
-                double truth = 0.25;
-                int count = 7;
+                double check = 0;
+   
                 do
                 {
-                    int countHere = brevnos.Where(x=>x.L>=check).Count();
+                    check = (check + max) / 2;
+                    check = Math.Round(check, 2);
+                    var countHere_ = brevnos.Select(x => (int)Math.Truncate(x.L / check));
+                    foreach (var item in countHere_) { info($"{item}"); }
+                    int countHere = countHere_.Sum();
                     info($"countHere: {countHere}\tcheck: {check}\tmax: {max}\tcount: {count}");
-                    if (countHere < count && isOpen) { max /= 2; check = 0; isOpen = false; }
-                    else if (countHere > count) { check = (check + max) / 2; isOpen = true; }
-                    ReadLine();
+                    if (countHere < count) { max = check; check = 0; };
+                    if (max - check < truth) { break;  }
+                    Error($"countHere: {countHere}\tcheck: {check}\tmax: {max}\tcount: {count}");
+                    //ReadLine();
                 }
                 while (true);
+                info($"Answer: {Math.Round((check+max)/2,2)}", ConsoleColor.Blue);
+                ReadLine();
             }
-            while(true) { Task1(); }
+            
+            void Task2()
+            {
+                Task2_Logic(new Random().Next(7, 1000));
+                Task2_Logic(new Random().Next(7, 1000));
+                Task2_Logic(new Random().Next(7, 1000));
+                Task2_Logic(new Random().Next(7, 1000));
+
+                Task2_Logic(inputInt("Take me N: "));
+                Task2_Logic(inputInt("Take me N: "));
+
+                void Task2_Logic(int N)
+                {
+                    int allCount = 0;
+                    int AndreyCost = 3;int SergeyCost = 5;
+                    int AndreyCount = 0; int SergeyCount = 0;
+                    while(allCount < N)
+                    {
+                        if (SergeyCount - AndreyCount == 1) { AndreyCount++; }
+                        else { SergeyCount++; }
+                        allCount = AndreyCost * AndreyCount + SergeyCost * SergeyCount;
+                    }
+                    info($"With N: {N}\nAndrey paid {AndreyCost}rubles X {AndreyCount}\tAndrey paid {SergeyCost}rubles X {SergeyCount}");
+                }
+            }
+
+            void Task3()
+            {
+                string GenerateString()
+                {
+                    string Empty = "1";
+                    foreach (var X in Enumerable.Range(1, 3000)) { WriteLine(X); Empty = $"{Empty}{X * 10}"; }
+                    return Empty;
+                }
+                info($"{GenerateString().Length}");
+            }
+
+            WorkList(new Action[]{
+                ()=>Task1(),
+                ()=>Task2(),
+                ()=>Task3()
+            });
             ReadLine();
 
         }
